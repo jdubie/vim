407d0d2c5d33b90cb4527a626372092b928b0eb4
Jack Dubie <jdubie@cs.stanford.edu>
2012-05-29 10:36:03 -0700

    updates

--- update.sh
+++ update.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+echo committing changes
+git add .
+git commit -a -m 'updates'
+git pull origin master
+git push origin master
+
 echo overwriting current vimrc
 cp ~/.vim/vimrc.vim-template ~/.vimrc
